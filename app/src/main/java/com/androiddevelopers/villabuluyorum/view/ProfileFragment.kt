4642f2ad8db608701b04bdbbc147976970d4aa62
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-05 02:36:30 +0300

    veritabanı bağlantısı için deneme

--- app/src/main/java/com/androiddevelopers/villabuluyorum/view/ProfileFragment.kt
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/view/ProfileFragment.kt
@@ -1,14 +1,19 @@
 package com.androiddevelopers.villabuluyorum.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.androiddevelopers.villabuluyorum.data.MySqlDbHelper
 import com.androiddevelopers.villabuluyorum.databinding.FragmentProfileBinding
 import com.androiddevelopers.villabuluyorum.viewmodel.ProfileViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
@@ -21,8 +26,40 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentProfileBinding.inflate(inflater,container,false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val scope = CoroutineScope(Dispatchers.IO)
+        scope.launch {
+            val list = mutableListOf<String>()
+            try {
+                val connection = MySqlDbHelper.connect(requireContext())
+                connection?.let {
+                    val statement = it.createStatement()
+                    val resultSet = statement.executeQuery("SELECT * FROM `users`")
+                    while (resultSet.next()) {
+                        val firstName = resultSet.getString("first_name")
+                        val lastName = resultSet.getString("last_name")
+                        list.add(firstName)
+                        list.add(lastName)
+                    }
+                }
+            } catch (e: Exception) {
+                e.localizedMessage?.let { message ->
+//                    Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+//                        .show()
+                }
+            }
+
+            requireActivity().runOnUiThread {
+                Snackbar.make(binding.root, list.toString(), Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 
     override fun onDestroy() {

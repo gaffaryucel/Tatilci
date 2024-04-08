e9f2a04860597c53d314d4202143585bb882285b
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-08 04:29:33 +0300

    argüman ve create facilities sayfası eklendi

---
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/viewmodel/home/HomeCreateFacilitiesViewModel.kt
@@ -0,0 +1,14 @@
+package com.androiddevelopers.villabuluyorum.viewmodel.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeCreateFacilitiesViewModel
+@Inject
+constructor(
+
+) : ViewModel() {
+    // TODO: Implement the ViewModel
+}

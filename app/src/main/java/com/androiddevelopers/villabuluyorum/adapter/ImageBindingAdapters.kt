9cb5752a4a82319cb02dea273c788d71dd159ac6
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-08 04:27:58 +0300

    resim indirme için binging adapter eklendi

---
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/adapter/ImageBindingAdapters.kt
@@ -0,0 +1,32 @@
+package com.androiddevelopers.villabuluyorum.adapter
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.androiddevelopers.villabuluyorum.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+//Bu üç metodu adaptör içinde databinding ile resimleri yüklemek için ekledik
+@BindingAdapter("downloadImage")
+fun downloadImage(view: ImageView, url: String?) {
+    view.imageDownload(url, view.context)
+}
+
+fun ImageView.imageDownload(url: String?, context: Context) {
+    val options =
+        RequestOptions()
+            .placeholder(createPlaceholder(context))
+            .error(R.drawable.placeholder)
+
+    Glide.with(context).setDefaultRequestOptions(options).load(url).into(this)
+}
+
+fun createPlaceholder(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}

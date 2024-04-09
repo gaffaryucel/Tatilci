95b1218180c85044471b9c5bb9f9845beb0edafc
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-09 03:42:19 +0300

    modelde düzeltme yapıldı

--- app/src/main/java/com/androiddevelopers/villabuluyorum/model/villa/Facilities.kt
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/model/villa/Facilities.kt
@@ -3,59 +3,13 @@ package com.androiddevelopers.villabuluyorum.model.villa
 import java.io.Serializable
 
 data class Facilities(
-    var id: Int? = null,
-    var landscape: Landscape? = null,
-    var bath: Bath? = null,
-    var bedroom: Bedroom? = null,
-    var entertainment: Entertainment? = null,
-    var heatingCooling: HeatingCooling? = null,
-    var kitchenFood: KitchenFood? = null,
-    var locationFeatures: LocationFeatures? = null,
-    var outdoor: Outdoor? = null,
-    var services: Services? = null
+    val landscape: MutableList<String> = mutableListOf(),
+    val bath: MutableList<String> = mutableListOf(),
+    val bedroom: MutableList<String> = mutableListOf(),
+    val entertainment: MutableList<String> = mutableListOf(),
+    val heatingCooling: MutableList<String> = mutableListOf(),
+    val kitchenFood: MutableList<String> = mutableListOf(),
+    val locationFeatures: MutableList<String> = mutableListOf(),
+    val outdoor: MutableList<String> = mutableListOf(),
+    val services: MutableList<String> = mutableListOf()
 ) : Serializable
-
-data class Landscape(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class Bath(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class Bedroom(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class Entertainment(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class HeatingCooling(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class KitchenFood(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class LocationFeatures(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class Outdoor(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)
-
-data class Services(
-    var imageResourceId: Int? = null,
-    var feature: String? = null,
-)

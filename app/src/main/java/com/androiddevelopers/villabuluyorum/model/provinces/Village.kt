81592cbae07c55b84396254986930c5b52ff1d76
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-13 12:41:53 +0300

    il, ilçe, mahalle ve köy için modeller oluşturuldu

---
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/model/provinces/Village.kt
@@ -0,0 +1,15 @@
+package com.androiddevelopers.villabuluyorum.model.provinces
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "villages")
+data class Village(
+    @PrimaryKey
+    val id: Int?,
+    val name: String?,
+    val districtId: Int?,
+    val district: String?,
+    val provinceId: Int?,
+    val province: String?,
+)

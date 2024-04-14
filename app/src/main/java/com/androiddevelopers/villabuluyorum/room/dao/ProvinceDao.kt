8b2df63036560478e97188c7ee45ea03b92ac192
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-15 00:38:54 +0300

    room veritabanından sorgu sonuçlarını işlemek için suspend yerine flow kullanıldı

--- app/src/main/java/com/androiddevelopers/villabuluyorum/room/dao/ProvinceDao.kt
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/room/dao/ProvinceDao.kt
@@ -6,18 +6,19 @@ import com.androiddevelopers.villabuluyorum.model.provinces.District
 import com.androiddevelopers.villabuluyorum.model.provinces.Neighborhood
 import com.androiddevelopers.villabuluyorum.model.provinces.Province
 import com.androiddevelopers.villabuluyorum.model.provinces.Village
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ProvinceDao {
-    @Query("SELECT * FROM provinces")
-    suspend fun getAllProvince(): List<Province>
+    @Query("SELECT * FROM provinces ORDER BY name COLLATE LOCALIZED ASC")
+    fun getAllProvince(): Flow<List<Province>>
 
-    @Query("SELECT * FROM districts WHERE provinceId = :provinceId")
-    suspend fun getAllDistrictById(provinceId: Int): List<District>
+    @Query("SELECT * FROM districts WHERE provinceId = :provinceId ORDER BY name COLLATE LOCALIZED ASC")
+    fun getAllDistrictById(provinceId: Int): Flow<List<District>>
 
-    @Query("SELECT * FROM neighborhoods WHERE districtId = :districtId")
-    suspend fun getAllNeighborhoodById(districtId: Int): List<Neighborhood>
+    @Query("SELECT * FROM neighborhoods WHERE districtId = :districtId ORDER BY name COLLATE LOCALIZED ASC")
+    fun getAllNeighborhoodById(districtId: Int): Flow<List<Neighborhood>>
 
-    @Query("SELECT * FROM villages WHERE districtId = :districtId")
-    suspend fun getAllVillageById(districtId: Int): List<Village>
+    @Query("SELECT * FROM villages WHERE districtId = :districtId ORDER BY name COLLATE LOCALIZED ASC")
+    fun getAllVillageById(districtId: Int): Flow<List<Village>>
 }

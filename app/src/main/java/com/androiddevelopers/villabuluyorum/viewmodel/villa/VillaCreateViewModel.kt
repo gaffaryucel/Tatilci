7a1eb49e4283a2c59b1e6d7634315536c79c7dd6
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-15 00:41:31 +0300

    villa ekleme için son işlemler yapıldı, resim ekleme işlemleri devam ediyor

--- app/src/main/java/com/androiddevelopers/villabuluyorum/viewmodel/villa/VillaCreateViewModel.kt
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/viewmodel/villa/VillaCreateViewModel.kt
@@ -9,12 +9,14 @@ import com.androiddevelopers.villabuluyorum.model.provinces.District
 import com.androiddevelopers.villabuluyorum.model.provinces.Neighborhood
 import com.androiddevelopers.villabuluyorum.model.provinces.Province
 import com.androiddevelopers.villabuluyorum.model.provinces.Village
+import com.androiddevelopers.villabuluyorum.model.villa.Villa
 import com.androiddevelopers.villabuluyorum.repo.FirebaseRepoInterFace
 import com.androiddevelopers.villabuluyorum.repo.RoomProvinceRepo
 import com.androiddevelopers.villabuluyorum.util.Resource
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -30,39 +32,70 @@ class VillaCreateViewModel
     val liveDataFirebaseStatus: LiveData<Resource<Boolean>>
         get() = _liveDataFirebaseStatus
 
-    private var _liveDataFirebaseProvince = MutableLiveData<List<Province>>()
-    val liveDataFirebaseProvince: LiveData<List<Province>>
-        get() = _liveDataFirebaseProvince
+    private var _liveDataProvinceFromRoom = MutableLiveData<List<Province>>()
+    val liveDataProvinceFromRoom: LiveData<List<Province>>
+        get() = _liveDataProvinceFromRoom
 
-    private var _liveDataFirebaseDistrict = MutableLiveData<List<District>>()
-    val liveDataFirebaseDistrict: LiveData<List<District>>
-        get() = _liveDataFirebaseDistrict
+    private var _liveDataDistrictFromRoom = MutableLiveData<List<District>>()
+    val liveDataDistrictFromRoom: LiveData<List<District>>
+        get() = _liveDataDistrictFromRoom
 
-    private var _liveDataFirebaseNeighborhood = MutableLiveData<List<Neighborhood>>()
-    val liveDataFirebaseNeighborhood: LiveData<List<Neighborhood>>
-        get() = _liveDataFirebaseNeighborhood
+    private var _liveDataNeighborhoodFromRoom = MutableLiveData<List<Neighborhood>>()
+    val liveDataNeighborhoodFromRoom: LiveData<List<Neighborhood>>
+        get() = _liveDataNeighborhoodFromRoom
 
-    private var _liveDataFirebaseVillage = MutableLiveData<List<Village>>()
-    val liveDataFirebaseVillage: LiveData<List<Village>>
-        get() = _liveDataFirebaseVillage
+    private var _liveDataVillageFromRoom = MutableLiveData<List<Village>>()
+    val liveDataVillageFromRoom: LiveData<List<Village>>
+        get() = _liveDataVillageFromRoom
 
     private var _liveDataFirebaseUser = MutableLiveData<UserModel>()
     val liveDataFirebaseUser: LiveData<UserModel>
         get() = _liveDataFirebaseUser
 
 
-    fun getAllProvince() = viewModelScope.launch(Dispatchers.IO) {
-        _liveDataFirebaseProvince.value = roomProvinceRepo.getAllProvince()
+    fun getAllProvince() = viewModelScope.launch {
+        roomProvinceRepo.getAllProvince().flowOn(Dispatchers.IO).collect {
+            _liveDataProvinceFromRoom.value = it
+        }
+
     }
 
-    fun getAllDistrictById(provinceId: Int) = viewModelScope.launch(Dispatchers.IO) {
-        _liveDataFirebaseDistrict.value = roomProvinceRepo.getAllDistrictById(provinceId)
+    fun getAllDistrictById(provinceId: Int) = viewModelScope.launch {
+        roomProvinceRepo.getAllDistrictById(provinceId).flowOn(Dispatchers.IO).collect {
+            _liveDataDistrictFromRoom.value = it
+        }
     }
 
-    fun getAllNeighborhoodAndVillageById(districtId: Int) = viewModelScope.launch(Dispatchers.IO) {
-        _liveDataFirebaseNeighborhood.value = roomProvinceRepo.getAllNeighborhoodById(districtId)
-        _liveDataFirebaseVillage.value = roomProvinceRepo.getAllVillageById(districtId)
+    fun getAllNeighborhoodById(districtId: Int) = viewModelScope.launch {
+        roomProvinceRepo.getAllNeighborhoodById(districtId).flowOn(Dispatchers.IO).collect {
+            _liveDataNeighborhoodFromRoom.value = it
+        }
     }
+
+    fun getAllVillageById(districtId: Int) = viewModelScope.launch {
+        roomProvinceRepo.getAllVillageById(districtId).flowOn(Dispatchers.IO).collect {
+            _liveDataVillageFromRoom.value = it
+        }
+    }
+
+    fun addVillaToFirestore(
+        villaId: String,
+        villa: Villa
+    ) = viewModelScope.launch {
+        _liveDataFirebaseStatus.value = Resource.loading(true)
+
+        firebaseRepo.addVillaToFirestore(villaId, villa)
+            .addOnSuccessListener {
+                _liveDataFirebaseStatus.value = Resource.loading(false)
+                _liveDataFirebaseStatus.value = Resource.success(true)
+            }.addOnFailureListener {
+                _liveDataFirebaseStatus.value = Resource.loading(false)
+                it.message?.let { message ->
+                    _liveDataFirebaseStatus.value = Resource.error(message)
+                }
+            }
+    }
+
 
     /*
 

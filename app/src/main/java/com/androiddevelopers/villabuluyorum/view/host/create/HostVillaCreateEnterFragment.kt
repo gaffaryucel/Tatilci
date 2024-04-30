f2b071622a8384efe07ffcd410d866ba1701a442
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-04-30 22:09:52 +0300

    villa create sayfasına yönlendirme sağlandı

--- app/src/main/java/com/androiddevelopers/villabuluyorum/view/host/create/HostVillaCreateEnterFragment.kt
+++ app/src/main/java/com/androiddevelopers/villabuluyorum/view/host/create/HostVillaCreateEnterFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.androiddevelopers.villabuluyorum.databinding.FragmentHostVillaCreateEnterBinding
 import com.androiddevelopers.villabuluyorum.viewmodel.host.create.HostVillaCreateEnterViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,6 +23,16 @@ class HostVillaCreateEnterFragment : Fragment() {
     ): View {
         _binding = FragmentHostVillaCreateEnterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        binding.cardHostVillaCreateEnter.setOnClickListener {
+            val directions =
+                HostVillaCreateEnterFragmentDirections.actionNavigationHostVillaCreateEnterToHostVillaCreateFragment()
+            Navigation.findNavController(it).navigate(directions)
+        }
     }
 
     override fun onDestroy() {
